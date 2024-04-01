de06f730d3aaf6cb53235e8c1d89f8c455ba635f
Hugo Martinet <59569523+hugomartinet@users.noreply.github.com>
2024-04-01 19:17:03 +0200

    :technologist: Make server reachable on local network in dev env

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -6,6 +6,10 @@ import Vapor
 
 // configures your application
 public func configure(_ app: Application) async throws {
+  if app.environment == .development {
+    app.http.server.configuration.hostname = "0.0.0.0"
+  }
+
   // uncomment to serve files from /Public folder
   // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
